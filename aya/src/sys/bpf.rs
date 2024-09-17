da9e11654c6d804a49a778492e7901abedabe01c
Gabriel Dolberg <gabriel@pelanor.io>
2024-09-17 14:08:05 +0300

    my btf loade always use explicit log level of 2

--- aya/src/sys/bpf.rs
+++ aya/src/sys/bpf.rs
@@ -635,7 +635,7 @@ pub(crate) fn bpf_load_btf_gb(
     u.btf = raw_btf.as_ptr() as *const _ as u64;
     u.btf_size = mem::size_of_val(raw_btf) as u32;
     if !log_buf.is_empty() {
-        u.btf_log_level = verifier_log_level.bits();
+        u.btf_log_level = 2;
         u.btf_log_buf = log_buf.as_mut_ptr() as u64;
         u.btf_log_size = log_buf.len() as u32;
     }
